2eee569c03b1e9d32e12e356053fc48d552a14b7
Katherine Carlile <kcarlile@mitre.org>
2023-12-08 21:04:20 +0000

    Moved mappings.py and output to /scripts

--- scripts/mappings.py
+++ scripts/mappings.py
@@ -1,19 +1,33 @@
 from mitreattack.stix20 import MitreAttackData
-import json
 
-mitre_attack_data = MitreAttackData('enterprise-attack-14.1.json')
+import pandas as pd
 
-insider_threat_file = open('insider-threat-ttp-kb.json')
-insider_threat_dict = json.load(insider_threat_file)
-print(insider_threat_dict)
+def main():
+    #Download https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json
+    #to ../scripts
+    mitre_attack_data = MitreAttackData("../enterprise-attack-14.1.json")
+    insider_threat_ttps = pd.read_csv('../insider-threat-ttp-kb.csv')
 
-insider_threat_techniques = insider_threat_dict['techniques']
+    # get data components detecting insider threat technique
+    output_dict = {}
+    for technique_id in insider_threat_ttps['Technique ID']:
+        technique = mitre_attack_data.get_object_by_attack_id(technique_id, 'attack-pattern')
+        datacomponents_detecting_technique = mitre_attack_data.get_datacomponents_detecting_technique(technique.id)
+        mitigations_mitigating_technique = mitre_attack_data.get_mitigations_mitigating_technique(technique.id)
 
-insider_threat_mitigations = []
-for insider_threat_technique in insider_threat_techniques[:5]:
-    print(insider_threat_technique['techniqueID'])
-    insider_threat_mitigations.append(m for m in mitre_attack_data.get_mitigations_mitigating_technique(mitre_attack_data.get_object_by_attack_id(insider_threat_technique['techniqueID'], 'attack-pattern')['id']))
-    insider_threat_datasources = mitre_attack_data.get_object_by_attack_id(insider_threat_technique['techniqueID'], 'attack-pattern')['x_mitre_data_sources']
+        output_dict[technique_id] = [technique.name]
 
-for mitigation in insider_threat_mitigations:
-    print(mitigation)
+        for datacomponent in datacomponents_detecting_technique:
+            datasource = mitre_attack_data.get_object_by_stix_id(datacomponent["object"].x_mitre_data_source_ref)
+            print(mitre_attack_data.get_attack_id(datasource.id)) #this is DS####
+            output_dict[technique_id].append([mitre_attack_data.get_attack_id(datasource.id), datasource.name])
+
+        for mitigation in mitigations_mitigating_technique:
+            print(mitre_attack_data.get_attack_id(mitigation["object"].id)) #this is M####
+            output_dict[technique_id].append([mitre_attack_data.get_attack_id(mitigation["object"].id), mitigation["object"].name])
+
+    output_df = pd.DataFrame.transpose(pd.DataFrame.from_dict(output_dict, orient='index'))
+    output_df.to_csv('output.csv')
+
+if __name__ == "__main__":
+    main()
